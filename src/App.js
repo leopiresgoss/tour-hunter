a5be4a73d751b58415769ff8d087baf773df3fed
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-07-07 16:36:56 -0600

    Add Container and Calendar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Calendar from './components/Calendar';
 
 function App() {
   return (
     <div className="App">
+      <Calendar />
       <Routes>
         <Route path="/" element={<div>Hello</div>} />
       </Routes>

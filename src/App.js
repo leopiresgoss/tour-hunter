aa9efa7a396cb65bcfb5c8dba92f877089549972
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-07-08 06:21:11 -0600

    Remove Calendar from App.js and update linters file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Calendar from './components/Calendar';
 
 function App() {
   return (
     <div className="App">
-      <Calendar />
       <Routes>
         <Route path="/" element={<div>Hello</div>} />
       </Routes>

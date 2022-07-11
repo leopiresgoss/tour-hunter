0eb09ee486f5633faedc9f7656bdaafac0d9bf40
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-07-11 10:34:57 -0600

    Add delete tour forms

--- src/pages/DeleteTour.js
+++ src/pages/DeleteTour.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import Button from '../components/Button';
 
-const DeleteTour = () => (
-  <section>
-    <h1>Delete a Tour</h1>
-    <div>
-      <span>1</span>
-      <p>title</p>
-      <Button btnName="Delete" btnType="submit" bgColor="bg-red text-white" />
-    </div>
-  </section>
-);
+const DeleteTour = () => {
+  const tours = [
+    {
+      id: 1,
+      title: 'Tour 1',
+    },
+    {
+      id: 2,
+      title: 'Tour 1',
+    },
+    {
+      id: 3,
+      title: 'Tour 1',
+    },
+    {
+      id: 4,
+      title: 'Tour 1',
+    },
+    {
+      id: 5,
+      title: 'Tour 1',
+    },
+  ];
+
+  const deleteTour = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <h1>Delete a Tour</h1>
+      <div>
+        {tours.map((tour) => (
+          <form key={tour.id} onSubmit={deleteTour}>
+            <span>{tour.id}</span>
+            <p>{tour.title}</p>
+            <Button btnName="Delete" btnType="submit" bgColor="bg-red text-white" />
+          </form>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default DeleteTour;

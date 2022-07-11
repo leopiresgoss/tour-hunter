29b34b8d24d55d008dcbfed5552292437ea619cf
Thi-Ponce <thi.pponce@gmail.com>
2022-07-11 12:37:15 -0300

    implement my reservations skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ReservationDetails from './pages/ReservationDetails';
-// import Homepage from './components/Homepage';
 import SideNav from './components/SideNav';
 import ReserveForm from './pages/ReserveForm';
 import TopNav from './components/TopNav';
+import MyReservations from './pages/MyReservations';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
 
       <SideNav />
       <Routes>
-        {/* <Route path="/" element={<Homepage />} /> */}
+        <Route path="/" element={<MyReservations />} />
 
         <Route path="/reservations/:id" element={<ReservationDetails />} />
         <Route path="/reserve-tour/:id" element={<ReserveForm />} />
--- src/pages/ReserveForm.js
+++ src/pages/ReserveForm.js
@@ -10,7 +10,6 @@ const ReserveForm = () => {
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen bg-green overflow-hidden bg-opacity-80">
-
       <img
         className="w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10"
         src="https://www.fanabc.com/english/wp-content/uploads/2021/08/Tourism-Danakil-Depression.jpg"
@@ -18,15 +17,16 @@ const ReserveForm = () => {
       />
       <TopNav />
       <div className="flex flex-col mx-auto justify-center align-middle">
-
-        <h2 className="text-2xl mx-auto font-bold uppercase text-white mb-3 tracking-wide md:tracking-widest">BOOK YOUR TOUR</h2>
+        <h2 className="text-2xl mx-auto font-bold uppercase text-white mb-3 tracking-wide md:tracking-widest">
+          BOOK YOUR TOUR
+        </h2>
         <hr className="border-gray w-1/2 mx-auto mb-3" />
         <p className="mx-auto text-white text-center w-full p-2 md:w-3/5 mb-10 text-sm font-semibold leading-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim
-          ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua..
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua..
         </p>
 
         <form
@@ -43,7 +43,6 @@ const ReserveForm = () => {
           />
         </form>
       </div>
-
     </div>
   );
 };

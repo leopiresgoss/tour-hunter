ed9bb566807b40ee1150c5b80a80e7b70b6869bc
mengiefen <mengiefen@gmail.com>
2022-07-13 23:51:08 +0300

    fix the signin api call

--- src/api/SignIn.js
+++ src/api/SignIn.js
@@ -1,16 +1,22 @@
+import { CHANGE_USER_STATUS, GET_USER_DATA } from '../redux/reducers/token';
+import store from '../redux/store';
+
 const SignInUser = async (formData) => {
+  store.dispatch(CHANGE_USER_STATUS('LOGGING'));
   const myHeaders = new Headers();
   myHeaders.append('Content-Type', 'application/json');
-  await fetch('https://tourhunterapp.herokuapp.com/users/sign_in', {
+  const response = await fetch('http://127.0.0.1:3000/users/sign_in', {
     method: 'POST',
     headers: myHeaders,
     body: JSON.stringify({ user: formData }),
-  })
-    .then((response) => response).then(
-      (result) => {
-        console.log(result.headers.Authorization);
-      },
-    );
+  });
+
+  const token = await [...response.headers][0][1];
+  const result = await response.json();
+  const userData = { role: result.role, token };
+  localStorage.setItem('user', JSON.stringify(userData));
+  store.dispatch(GET_USER_DATA(userData));
+  store.dispatch(CHANGE_USER_STATUS('LOGGED'));
 };
 
 export default SignInUser;

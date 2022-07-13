f9e45d081f10f38a0d05fd5249a0ca11ebb356f9
mengiefen <mengiefen@gmail.com>
2022-07-13 23:50:32 +0300

    update the token reducer

--- src/redux/reducers/token.js
+++ src/redux/reducers/token.js
@@ -1,19 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
-/* eslint no-param-reassign: "error" */
 
+/* eslint no-param-reassign: "error" */
 export const tokenSlice = createSlice({
   name: 'token',
   initialState: {
-    token: null,
+    userData: {
+      token: null,
+      role: 'guest',
+    },
     signIn_status: 'Not Signed In',
-    role: 'guest',
   },
   reducers: {
-    GET_TOKEN: (state, action) => {
-      state.package = action.payload;
+    GET_USER_DATA: (state, action) => {
+      state.userData = action.payload;
+    },
+    CHANGE_USER_STATUS: (state, action) => {
+      state.signIn_status = action.payload;
     },
   },
 });
 
-export const { GET_TOKEN } = tokenSlice.actions;
+export const { GET_USER_DATA, CHANGE_USER_STATUS } = tokenSlice.actions;
 export default tokenSlice.reducer;

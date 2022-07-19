af13629f14ddcf4d668551ea033aca02913f99e5
behnam1369 <behnam.aghaali@yahoo.com>
2022-07-20 02:24:33 +0430

    fix NavButton functionality on the home page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
@@ -13,11 +13,14 @@ import {
 
 function Homepage() {
   const signedIn = useSelector((state) => state.signedIn);
+  const status = useSelector((state) => state.home.status);
   const dispatch = useDispatch();
   // eslint-disable-next-line max-len
   const tours = useSelector((state) => state.home.tours.filter(
     (t) => t.visible === true || window.screen.width < 500,
   ));
+
+  const [startPostition, setStartPosition] = useState(0);
   useEffect(() => {
     dispatch(homepageTourAPI());
   }, []);
@@ -32,28 +35,61 @@ function Homepage() {
       <div className="w-full flex flex-col gap-24 md:flex-row  md:justify-center md:items-center md:min-h-[80vh] md:gap-0">
         <div className="hidden md:block">
           <button
-            onClick={() => dispatch(updateLeftTours())}
+            onClick={() => {
+              if (startPostition > 0) {
+                dispatch(updateLeftTours());
+                setStartPosition(startPostition - 1);
+              }
+            }}
             type="button"
-            className="bg-green text-white max-w-[100px] py-3 pl-8 pr-3 rounded-br-full rounded-tr-full font-semibold"
+            className={`${status === 'Loading' || startPostition === 0 ? 'bg-gray' : 'bg-green'} text-white max-w-[100px] py-3 pl-8 pr-3 rounded-br-full rounded-tr-full font-semibold`}
           >
             <BiLeftArrow />
           </button>
         </div>
 
-        {tours.map((tour) => (
-          <Link key={tour.id} to={`/tour/${tour.id}`}>
-            <TourCard
-              tourName={tour.name}
-              tourLocale={tour.location}
-              tourImage={tour.image_urls[0]}
-            />
-          </Link>
-        ))}
+        {
+          status === 'Loading'
+            ? (
+              <>
+                <TourCard
+                  tourName="loading ..."
+                  tourLocale="loading ..."
+                  tourImage="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Blank_file.xcf/640px-Blank_file.xcf.png"
+                />
+                <TourCard
+                  tourName="loading ..."
+                  tourLocale="loading ..."
+                  tourImage="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Blank_file.xcf/640px-Blank_file.xcf.png"
+                />
+                <TourCard
+                  tourName="loading ..."
+                  tourLocale="loading ..."
+                  tourImage="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Blank_file.xcf/640px-Blank_file.xcf.png"
+                />
+              </>
+            )
+            : tours.map((tour) => (
+              <Link key={tour.id} to={`/tour/${tour.id}`}>
+                <TourCard
+                  tourName={tour.name}
+                  tourLocale={tour.location}
+                  tourImage={tour.image_urls[0]}
+                />
+              </Link>
+            ))
+
+        }
         <div className="hidden md:block">
           <button
-            onClick={() => dispatch(updateRightTours())}
+            onClick={() => {
+              if (startPostition < tours.length) {
+                dispatch(updateRightTours());
+                setStartPosition(startPostition + 1);
+              }
+            }}
             type="button"
-            className="bg-green text-white max-w-[100px] py-3 pl-3 pr-8 rounded-bl-full rounded-tl-full  font-semibold mx-5"
+            className={`${status === 'Loading' || startPostition === tours.length ? 'bg-gray' : 'bg-green'} text-white max-w-[100px] py-3 pl-3 pr-8 rounded-bl-full rounded-tl-full  font-semibold mx-5`}
           >
             <BiRightArrow />
           </button>
